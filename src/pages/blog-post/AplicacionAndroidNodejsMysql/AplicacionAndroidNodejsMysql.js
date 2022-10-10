7e2f1656ffda905f03fa6634e53fe88ca9246d2a
Juan Diego Cobo <juandiego14012003@gmail.com>
2022-10-10 00:24:59 -0500

    Making more confortable with deleting images the reading of the post

--- src/pages/blog-post/AplicacionAndroidNodejsMysql/AplicacionAndroidNodejsMysql.js
+++ src/pages/blog-post/AplicacionAndroidNodejsMysql/AplicacionAndroidNodejsMysql.js
@@ -44,10 +44,16 @@ const AplicacionAndroidNodejsMysql = () => {
         Para iniciar, creamos una carpeta donde se almacenará tanto backend (servidor donde se incluirá
         el script de creación de la base de datos) y la aplicacion android.
       </p>
-      
-      <Container className="text-center" fluid>
-        <img className="w-50" src={process.env.PUBLIC_URL + "/septimo-semestre/desarrollo-moviles/aplicacion-android-nodejs-mysql/imgs/carpetas-proyecto.PNG"} />
-      </Container>
+
+      <pre>
+        <code className="language-plaintext">
+          {
+            "aplicacion-full-stack\n" +
+            "  /app\n" +
+            "  /backend"
+          }
+        </code>
+      </pre>
 
       <p className="mt-3">
         Luego, cuando estemos creando el proyecto en Android Studio, haremos referencia a la carpeta que 
@@ -64,9 +70,17 @@ const AplicacionAndroidNodejsMysql = () => {
         carpeta de <i>layouts:</i>
       </p>
 
-      <Container className="text-center" fluid>
-        <img className="w-50" src={process.env.PUBLIC_URL + "/septimo-semestre/desarrollo-moviles/aplicacion-android-nodejs-mysql/imgs/archivos-layouts.PNG"} />
-      </Container>
+      <pre>
+        <code className="language-plaintext">
+        {
+            "res\n" +
+            "  /drawable\n" +
+            "  /layout\n" +
+            "    ../activity_main.xml\n" +
+            "    ../activity_user.xml"
+          }
+        </code>
+      </pre>
 
       <p className="mt-3">
         En el <i>activity main</i> vamos a crear un inicio de sesión simple con un número de cédula y
@@ -82,10 +96,25 @@ const AplicacionAndroidNodejsMysql = () => {
           Para cambiar el tamaño de la fuente de texto debemos agregar la propiedad en el código
           del activity main de la siguiente manera:
       </p>
-
-      <Container className="text-center" fluid>
-        <img className="w-50" src={process.env.PUBLIC_URL + "/septimo-semestre/desarrollo-moviles/aplicacion-android-nodejs-mysql/imgs/text-size.PNG"} />
-      </Container>
+      
+      <pre>
+        <code className="language-xml">
+          {
+            "<TextView\n" +
+            '  android:id="@+id/textView"\n' +
+            '  android:layout_width="wrap_content"\n' +
+            '  android:layout_height="wrap_content"\n' +
+            '  android:text="Cédula"\n' +
+            '  android:textSize="30dp"\n' +
+            '  app:layout_constraintBottom_toBottomOf="parent"\n' +
+            '  app:layout_constraintEnd_toEndOf="parent"\n' +
+            '  app:layout_constraintHorizontal_bias="0.047"\n' +
+            '  app:layout_constraintStart_toStartOf="parent"\n' +
+            '  app:layout_constraintTop_toTopOf="parent"\n' +
+            '  app:layout_constraintVertical_bias="0.052" />\n'
+          }
+        </code>
+      </pre>
 
       <p className="mt-3">
           Para efectos de prueba, el activity de user tendrá un mensaje de bienvenida hasta que se logre
@@ -110,53 +139,76 @@ const AplicacionAndroidNodejsMysql = () => {
         establecemos las características del proyecto.
       </p>
 
-      <Container className="text-center" fluid>
-        <img className="w-50" src={process.env.PUBLIC_URL + "/septimo-semestre/desarrollo-moviles/aplicacion-android-nodejs-mysql/imgs/npm-init.PNG"} />
-      </Container>
-
       <p className="mt-3">
         Para instalar express ejecutamos:<br />
-        <code>
+
+        <pre>
+        <code className="language-cmd">
           npm i express
-        </code><br/>
-        y para el driver de mysql:<br />
-        <code>
+        </code>
+        </pre>
+
+        y para el driver de mysql:
+
+        <pre>
+        <code className="language-cmd">
           npm i mysql
         </code>
+        </pre>
+        
       </p>
 
-      <Container className="text-center" fluid>
-        <img className="w-50" src={process.env.PUBLIC_URL + "/septimo-semestre/desarrollo-moviles/aplicacion-android-nodejs-mysql/imgs/npm-install.PNG"} />
-      </Container>
-
       <p className="mt-3">
-        Posteriormente, creamos el archivo <i>index.js</i> donde estableceremos la conexión y las rutas
-        donde se harán las peticiones HTTP. Para ello, en primer lugar debemos importar las 
+        Posteriormente, creamos el archivo <i>index.js</i> donde estableceremos la conexión, las rutas
+        donde se harán las peticiones HTTP y características adicionales que necesita el servidor 
+        para poder decodificar las peticiones. Para ello, en primer lugar debemos importar las 
         librerías instaladas utilizando, para este caso, la palabra reservada <code>require</code> y 
-        declarar el puerto donde escuchará nuestra aplicación de esta manera:
+        declarar el puerto donde escuchará nuestra aplicación. Luego agregamos las características 
+        mencionadas:
       </p>
 
-      <Container className="text-center" fluid>
-        <img className="w-50" src={process.env.PUBLIC_URL + "/septimo-semestre/desarrollo-moviles/aplicacion-android-nodejs-mysql/imgs/require-index.PNG"} />
-      </Container>
+      <pre>
+        <code className="language-javascript">
+          {
+            "const express = require('express')\n" +
+            "const mysql = require('mysql')\n" +
+            "const app = express()\n" +
+            "const port = 3001\n\n" +
+            "app.use(express.json())\n" +
+            "app.use(express.urlencoded({extended: false}))"
+          }
+        </code>
+      </pre>
 
       <p className="mt-3">
-        La declaración de la constante <i>app</i> sirve para hacer referencia al servidor en cuestión.
-        <br /><br />
+        La declaración de la constante <i>app</i> sirve para hacer referencia al servidor en cuestión. 
         Luego de ello, configuramos la conexión a MySQL:
       </p>
 
-      <Container className="text-center" fluid>
-        <img className="w-50" src={process.env.PUBLIC_URL + "/septimo-semestre/desarrollo-moviles/aplicacion-android-nodejs-mysql/imgs/conexion-mysql-index.PNG"} />
-      </Container>
+      <pre>
+        <code className="language-javascript">
+          {
+            "// MySQL connection\n" +
+            "const connection = mysql.createConnection({\n" +
+            "    host: 'localhost',\n" +
+            "    user: 'root',\n" +
+            "    password: 'root',\n" +
+            "    database: 'app_db'\n" +
+            "});\n\n" +
+
+            "// For testing connection\n" +
+            "connection.connect();\n"
+          }
+        </code>
+      </pre>
       
-      <p className="mt-3">
+      <p>
         <b>Nota</b><br />
         Los valores del host, user, password y database deben ser cambiados a como se hayan denominado,
         es decir, en el caso de mi equipo, mi usuario y contraseña son <i>'root'</i> y estaré corriendo
         el servidor de MySQL en el localhost en el <b>puerto 3306</b> (valor que viene por defecto en 
         el conector de mysql). Por tal razón, en caso de tener MySQL corriendo en otro puerto se deberá
-        agregar un nuevo par llave-valor que sea:<br /><br />
+        agregar un nuevo par llave-valor que sea:<br />
         <pre>
           <code className="language-plaintext">
               port: número del puerto en cuestión
@@ -164,24 +216,43 @@ const AplicacionAndroidNodejsMysql = () => {
         </pre>
       </p>
 
-      <p className="mt-3">
+      <p>
         Luego de ello, configuramos una ruta de prueba inicial que luego iremos modificando para agregar 
         cada uno de los métodos GET, POST, PUT y DELETE.
       </p>
+      
+      <pre>
+        <code className="language-javascript">
+          {
+            "// Routes\n" +
+            "app.route('/')\n\n" +
 
-      <Container className="text-center" fluid>
-        <img className="w-50" src={process.env.PUBLIC_URL + "/septimo-semestre/desarrollo-moviles/aplicacion-android-nodejs-mysql/imgs/ruta-inicial.PNG"} />
-      </Container>
+            "  .get((req, res) => {\n" +
+            "    res.json({\n" +
+            "      'code': 200,\n" +
+            "      'message': 'Hello World'\n" +
+            "    })\n" +
+            "  })\n"
+          }
+        </code>
+      </pre>
 
-      <p className="mt-3">
-        Finalmente, le indicamos al servidor que se mantenga escuchando en el puerto 3000, de esta manera:
+      <p>
+        Finalmente, le indicamos al servidor que se mantenga escuchando en el puerto 3001, de esta manera:
       </p>
 
-      <Container className="text-center" fluid>
-        <img className="w-50" src={process.env.PUBLIC_URL + "/septimo-semestre/desarrollo-moviles/aplicacion-android-nodejs-mysql/imgs/escuchando-puerto.PNG"} />
-      </Container>
+      <pre>
+          <code className="language-javascript">
+            {
+              "// Starting to listen\n" +
+              "app.listen(port, () => {\n" +
+              "  console.log('Server listen on port 3001...')\n" +
+              "})"
+            }
+          </code>
+      </pre>
 
-      <p className="mt-3">
+      <p>
         Antes de comprobar que esté funcionando debemos proceder realizar la creación de la base de
         datos.
       </p>
@@ -190,7 +261,7 @@ const AplicacionAndroidNodejsMysql = () => {
         Creación de la base de datos
       </h2>
       
-      <p className="">
+      <p>
         Para la creación de la base de datos emplearemos la consola de comandos y escribiremos todas las
         instrucciones en un archivo <i>.sql</i> que se guardará en la carpeta de <i>backend</i>.<br/><br/>
         
@@ -198,20 +269,38 @@ const AplicacionAndroidNodejsMysql = () => {
         y la creación de la tabla de los usuarios, de esta manera:
       </p>
 
-      <Container className="text-center" fluid>
-        <img className="w-50" src={process.env.PUBLIC_URL + "/septimo-semestre/desarrollo-moviles/aplicacion-android-nodejs-mysql/imgs/script-database.PNG"} />
-      </Container>
-
-      <Container className="text-center mt-3" fluid>
-        <img className="w-25" src={process.env.PUBLIC_URL + "/septimo-semestre/desarrollo-moviles/aplicacion-android-nodejs-mysql/imgs/results-script.PNG"} />
-      </Container>
+      <pre>
+        <code className="language-sql">
+        {
+          "-- Database creation\n" +
+          "CREATE DATABASE app_db;\n\n" +
+          
+          "USE app_db;\n\n" +
+          
+          "-- Table users creation\n" +
+          "CREATE TABLE USUARIO(\n" +
+          "    cedula INTEGER PRIMARY KEY,\n" +
+          "    contrasena VARCHAR(100),\n" +
+          "    nombre VARCHAR(100),\n" +
+          "    apellido VARCHAR(100),\n" +
+          "    telefono VARCHAR(20),\n" +
+          ");\n"
+        }
+        </code>
+      </pre>      
 
       <p className="mt-3">
         A su vez, lo que podríamos hacer sería insertar dos usuarios de prueba. Para tales efectos utilizamos
         las siguientes instrucciones dentro de la shell de MySQL:<br />
-        <code>INSERT INTO USUARIO VALUES(123, 'juan123', 'Juan', 'C.', '3101234446');</code><br/>
-        <code>INSERT INTO USUARIO VALUES(124, 'juan124', 'Diego', 'C.', '3111245556');</code><br/>
       </p>
+      <pre>
+        <code className="language-sql">
+          {
+            "INSERT INTO USUARIO VALUES(123, 'juan123', 'Juan', 'C.', '3101234446');\n" +
+            "INSERT INTO USUARIO VALUES(124, 'juan124', 'Diego', 'C.', '3111245556');"
+          }
+        </code>
+      </pre>
 
       <h2 className="h2 text-dark mt-5">
         Pruebas servidor - MySQL
@@ -233,7 +322,7 @@ const AplicacionAndroidNodejsMysql = () => {
         
         Para arreglar este error basta con ejecutar el siguiente comando en la terminal de MySQL:
         <pre>
-          <code className="language-plaintext">
+          <code className="language-sql">
             ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
           </code>
         </pre>
@@ -243,19 +332,16 @@ const AplicacionAndroidNodejsMysql = () => {
       </p>
 
       <p>
-        <b>Nota</b><br/>
-        En este punto se puede generar un error ya que se estará intentando correr diferentes aplicaciones
-        en el mismo puerto. Para ello, lo que podremos hacer es cambiar la constante del puerto
-        en el <i>index.js</i> y colocarlo en el <i>puerto 3001.</i>
-      </p>
-
-      <p>
         Así pues, cuando accedemos a la ruta de prueba, debemos visualizar el siguiente mensaje:
       </p>
       
-      <Container className="text-center mt-3" fluid>
-        <img className="w-50" src={process.env.PUBLIC_URL + "/septimo-semestre/desarrollo-moviles/aplicacion-android-nodejs-mysql/imgs/ruta-prueba.PNG"} />
-      </Container>
+      <pre>
+          <code className="language-json">
+            {
+              '{"code": 200, "message": "Hello World"}'
+            }
+          </code>
+      </pre>
 
       <p className="mt-3">
         Una vez comprobado que el servidor funciona, modificamos la ruta get para hacer una consulta a la
@@ -263,18 +349,70 @@ const AplicacionAndroidNodejsMysql = () => {
         <code> connection</code> que se creó para conectar MySQL con Nodejs y su método <code>query</code>.
         Estas modificaciones quedarían finalmente de la siguiente manera:
       </p>
+      
+      <pre>
+        <code className="language-javascript">
+          {
+            ".get((req, res) => {\n\n" +
 
-      <Container className="text-center mt-3" fluid>
-        <img className="w-50" src={process.env.PUBLIC_URL + "/septimo-semestre/desarrollo-moviles/aplicacion-android-nodejs-mysql/imgs/modificacion-ruta.PNG"} />
-      </Container>
+            '    let query_usuarios = "SELECT * FROM USUARIO";\n'+
+            "    connection.query(query_usuarios, (err, results, fields) => {\n\n" +
+            
+            "        if (err) {\n" +
+            '            console.log("There was an error");\n' +
+            "            console.log(err);\n" +
+            "            res.json({\n" +
+            "                    'code': 500,\n" +
+            "                    'message': 'There was an server error.'\n" +
+            "            });\n" +
+            "        } else {\n" +
+            "            let data = [];\n\n" +
+            
+            "            for (let i = 0; i < results.length; i++) {\n" +
+            "                data.push(results[i]);\n" +
+            "            }\n\n" +
+            
+            "            res.json({\n" +
+            "                    'code': 200,\n" +
+            "                    'message': 'Values got it with success',\n" +
+            "                    'data': data\n" +
+            "            });\n" +
+            "        }\n" +
+            "    })\n" +
+            "})"
+        }
+        </code>
+      </pre>      
 
       <p className="mt-3">
         Cuando accedemos a la ruta del servidor, deberíamos visualizar lo siguiente:
       </p>
 
-      <Container className="text-center mt-3" fluid>
-        <img className="w-50" src={process.env.PUBLIC_URL + "/septimo-semestre/desarrollo-moviles/aplicacion-android-nodejs-mysql/imgs/resultados-ruta-prueba.PNG"} />
-      </Container>
+      <pre>
+        <code className="language-json">
+          {
+            '{\n' +
+            '  "code":200,\n' +
+            '  "message":"Values got it with success",\n' +
+            '  "data":[\n' +
+            '    {\n' +
+            '      "cedula":123,\n' +
+            '      "contrasena":"juan123",\n' +
+            '      "nombre":"Juan",\n' +
+            '      "apellido":"C.",\n' +
+            '      "telefono":"3101234446"\n' +
+            '    },\n' +
+            '    {\n' +
+            '      "cedula":124,\n' +
+            '      "contrasena":"juan124",\n' +
+            '      "nombre":"Diego",\n' +
+            '      "apellido":"C.",\n' +
+            '      "telefono":"3111245556"}\n' +
+            '    ]\n' +
+            '  }\n'
+          }
+        </code>
+      </pre>
       
       <p className="mt-3">
         Si al acceder a la ruta visualizamos el json que se ve en la imagen anterior, podemos concluir
@@ -366,9 +504,13 @@ const AplicacionAndroidNodejsMysql = () => {
         y accedimos a <code>localhost:3001.</code> En mi caso, cuando hice click en el emulador visualicé el siguiente mensaje: <br /><br /> 
       </p>
 
-      <Container className="text-center mt-3" fluid>
-        <img className="w-75" src={process.env.PUBLIC_URL + "/septimo-semestre/desarrollo-moviles/aplicacion-android-nodejs-mysql/imgs/pruebas-android-servidor.PNG"} />
-      </Container>
+      <pre>
+        <code className="language-plaintext">
+          {
+            'I/data: {"code":200,"message":"Values got it with success","data":[{"cedula":123,"contrasena":"juan123","nombre":"Juan","apellido":"C.","telefono":"3101234446"},{"cedula":124,"contrasena":"juan124","nombre":"Diego","apellido":"C.","telefono":"3111245556"}]}'
+          }
+        </code>
+      </pre>
 
       <p className="mt-5">
         <b>Nota</b><br />
