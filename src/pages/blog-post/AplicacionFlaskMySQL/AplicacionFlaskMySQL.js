4f8c61a692c7ae80901e7924a346b1793b471741
unknown <juandiego14012003@gmail.com>
2022-10-18 20:25:45 -0500

    Adding explanations for article.html

--- src/pages/blog-post/AplicacionFlaskMySQL/AplicacionFlaskMySQL.js
+++ src/pages/blog-post/AplicacionFlaskMySQL/AplicacionFlaskMySQL.js
@@ -45,7 +45,7 @@ const AplicacionFlaskMySql = () => {
       </p>
 
       <pre>
-        <code className="bg-warning pt-3 pb-0 language-cmd">
+        <code className="bg-warning py-3 language-cmd">
           {
             "pip3 install Flask\n" +
             "pip3 install flask-mysqldb"
@@ -58,7 +58,7 @@ const AplicacionFlaskMySql = () => {
       </h2>
 
       <p>
-        Para iniciar creamos una carpeta llamada <code>flask-mysql</code> en la cual tendremos nuestro <code>Vagrant</code> file.
+        Para iniciar creamos una carpeta llamada <code>flask-mysql</code> en la cual tendremos nuestro <code>Vagrantfile.</code>{' '}
         Para ello ejecutamos los siguientes comandos desde la consola:
       </p>
 
@@ -73,11 +73,11 @@ const AplicacionFlaskMySql = () => {
       </pre>
 
       <p>
-        Una vez hemos ejecutado <code>vagrant init</code> tendremos un <code>Vagrant file</code> que lo modificamos para 
+        Una vez hemos ejecutado <code>vagrant init</code> tendremos un <code>Vagrantfile</code> que lo modificamos para 
         establecer una máquina Centos 8 con un hostname denominado como <code>server</code> (aunque puede definirse como se desee).
         También, se debe establecer una ip para poder visualizar los cambios desde nuestro pc anfitrión con la opción <code>network</code>{' '}
         de Vagrant.<br /><br />
-        Esta <code>Vagrant file</code> deberá lucir:
+        Esta <code>Vagrantfile</code> deberá lucir:
       </p>
 
       <pre>
@@ -511,7 +511,7 @@ const AplicacionFlaskMySql = () => {
       </p>
       
       <div className="bg-info p-3 p">
-        <span className="h4">¿Cómo agregar componentes <code>examples</code> de bootstrap?</span>
+        <h4 className="my-3">¿Cómo agregar componentes <code>examples</code> de bootstrap?</h4>
 
         <ol>
           <li>Ir a la página de <a href="https://getbootstrap.com/docs/5.2/examples/" target="_blank">examples</a> de bootstrap.</li>
@@ -523,8 +523,7 @@ const AplicacionFlaskMySql = () => {
         </ol>
       </div>
 
-
-      <p>
+      <p className="mt-3">
         El contenedor <code>{'<div id="content">'}</code> es el que utilzaremos como referencia para agregar
         todo el contenido de las demás páginas ya que solo cambiará el contenido, pero el <code>header</code>{' '}
         y el <code>footer</code> permanecerán igual (por esto le llamamos plantilla). Ahora, procederemos agregando algo
@@ -636,7 +635,164 @@ const AplicacionFlaskMySql = () => {
         se denominan en Bootstrap como <code>Modals.</code> En este{' '}
         <a href="https://getbootstrap.com/docs/5.2/components/modal/" target="_blank">link</a> podrán acceder a la guía de los modales.
       </p>
+      
+      <div>
+        <p>
+          Ahora bien, es importante tener en cuenta que el motor de renderizados de plantillas se denomina <code>jinja.</code> Este
+          nos permite ejecutar código python inmerso en html. Similar a lo vendría siendo <code>Handlebars</code>, <code>Java NET</code>, entre otros.
+          Con lo anterior, podemos por ejemplo, ejecutar ciclos que nos permitan renderizar múltiples elementos HTML enviados a través un arreglo.
+          En nuestro caso, lo que queremos es enviar un arreglo de todos los artículos que se obtienen a partir de la consulta a la base de
+          datos. Por ello, desde el archivo <code>views.py</code> tenemos que establecer la ruta inicial de los artículos de esta manera:
+        </p>
+
+        <pre>
+          <code className="language-python">
+            {
+              '# views.py\n' +
+              '# Route to get articles\n' +
+              "@app.route('/articles', methods=['GET', 'POST'])\n" +
+              'def articlesGet():\n' +
+              '    cur = mysql.connection.cursor()\n' +
+              '    cur.execute("SELECT * FROM `the_article`")\n' +
+              '    the_articles = cur.fetchall()\n\n' +
   
+              '    return render_template("articles.html", the_articles=the_articles)\n'
+            }
+          </code>
+        </pre>
+        
+        <p>
+          Lo que se hace en esta función es simplemente conectar Flask con MySQL, ejecutar la consulta de los artículos en la
+          base de datos y posteriormente retornarlos a la vista de <code>articles.hmtl.</code> Sin embargo, seguramente quede
+          la pregunta de ¿cómo conectar MySQL con Flask? Para ello, prosegurié explicando esta parte y luego continuaré con
+          explicando articles.
+        </p>
+      </div>
+
+      <div className="p-3 bg-info">
+        <h4 className="my-3">¿Cómo conectar Flask con MySQL?</h4>
+        <p>
+          Para conectar Flask con MySQL primero deberemos especificar en el archivo <code>config.py</code> el usuario,
+          la contraseña y la base de datos a la que nos conectaremos de esta manera:
+        </p>
+
+        <pre>
+          <code className="bg-info language-python">
+            {
+              '# config.py\n' +
+              '...\n' +
+              'MYSQL_USER="root"\n' +
+              'MYSQL_PASSWORD="root"\n' +
+              'MYSQL_DB="app_flask_db"'
+            }
+          </code>
+        </pre>
+
+        <p>
+          Claro está, se deben establecer la contraseña y los usuarios correspondientes a los de su máquina.
+          Posteriormente lo que se debe hacer es importar dichas configuraciones en el <code>__init__.py</code>
+          {' '}utilizando la librería que se instaló en un principió:
+        </p>
+
+        <pre>
+          <code className="bg-info language-python">
+            {
+              'from flask_mysqldb import MySQL\n\n' +
+              'app = Flask(__name__, instance_relative_config=True)\n' +
+              'app.config.from_object("config")'
+            }
+          </code>
+        </pre>
+
+        <p>
+          Luego, se le debe indicar a MySQL que utilice dicha configuración del app:
+        </p>
+
+        <pre>
+          <code className="bg-info language-python">
+            {
+              '...\n' +
+              'mysql = MySQL(app)'
+            }
+          </code>
+        </pre>
+
+        <p>
+          Y ya estaría. Ahora para utilizar constantemente los llamados a MySQL se debe importar, crear el cursor y ejecutar los querys:
+        </p>
+
+        <pre>
+          <code className="bg-info language-python">
+            {
+              'from app import ..., mysql\n' +
+              'cur = mysql.connection.cursor()\n' +
+              'cur.execute("SELECT * FROM `the_article`")'
+            }
+          </code>
+        </pre>
+
+        <p>
+          <b>Recordar</b> que cuando se estén realizando inserciones o actualizaciones se debe hacer un commit en la base de datos:
+        </p>
+
+        <pre>
+          <code className="bg-info language-python">
+            {
+              '...\n' +
+              'mysql.connection.commit()'
+            }
+          </code>
+        </pre>
+      </div>
+      
+      <div>
+        <h2 className="h2 text-dark mt-5">
+          Continuación de <code>articles.html</code>
+        </h2>
+        
+        <p>
+          Continuando con el código del template, tenemos que indicarle al HTML que renderizaremos una fila por cada artículo que se encuentre en la base de datos.
+          Esto se hace de la siguiente manera:
+        </p>
+
+        <pre>
+          <code className="language-html">
+            {
+              '{% for article in the_articles %}\n' +
+              '<div class="row text-center mx-5">\n' +
+              '    <div class="col p-3 border">\n' +
+              '        <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#updateArticle{{ article[0] }}">\n' +
+              '            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">\n' +
+              '                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>\n' +
+              '            </svg>\n' +
+              '        </button>\n\n' +
+
+              '        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteArticle{{ article[0] }}">\n' +
+              '            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">\n' +
+              '                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>\n' +
+              '            </svg>\n' +
+              '        </button>\n' +
+              '    </div>\n\n' +
+
+              '    <div class="col p-3 border d-flex justify-content-center align-items-center">{{ article[0] }}</div>\n' +
+              '    <div class="col p-3 border d-flex justify-content-center align-items-center">{{ article[1] }}</div>\n' +
+              '    <div class="col p-3 border d-flex justify-content-center align-items-center">{{ article[2] }}</div>\n' +
+              '    <div class="col p-3 border d-flex justify-content-center align-items-center">{{ article[3] }}</div>\n' +
+              '    <div class="col p-3 border d-flex justify-content-center align-items-center">{{ article[4] }}</div>\n' +
+              '</div>\n' +
+              '...\n' +
+              '{% endfor %}'
+            }
+          </code>
+        </pre>
+
+        <p>
+          Ahora bien, como se puede observar, empleamos código de python para indicarle que por cada dato del <code>article</code> queremos obtener el dato 1 
+          {' '}(<code>article[0]</code>,) el dato 2 (<code>article[1]</code>) y así sucesivamente.
+        </p>
+      </div>
+
+      
 
     </Container>
   </div>
