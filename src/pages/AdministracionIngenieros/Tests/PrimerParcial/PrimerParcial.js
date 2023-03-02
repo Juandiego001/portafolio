e23579b22de79979b5f9db1384e73aa7de4f82ef
Juandiego001 <juandiego14012003@gmail.com>
2023-03-01 19:48:44 -0500

    Adding Primer Parcial for Administracion para ingenieros

---
+++ src/pages/AdministracionIngenieros/Tests/PrimerParcial/PrimerParcial.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './PrimerParcial.module.css';
+
+const PrimerParcial = () => (
+  <div className={styles.PrimerParcial}>
+    PrimerParcial Component
+  </div>
+);
+
+PrimerParcial.propTypes = {};
+
+PrimerParcial.defaultProps = {};
+
+export default PrimerParcial;
