e6d6d99cddcf9292c9a61c5f55c729a4353bb70b
unknown <juandiego14012003@gmail.com>
2023-03-07 12:23:38 -0500

    Working on React + Typescript + MySQL for Proyecto Informatico 2

--- src/pages/ProyectoInformatico2/Posts/ReactTypeScriptMySQL/ReactTypeScriptMySQL.js
+++ src/pages/ProyectoInformatico2/Posts/ReactTypeScriptMySQL/ReactTypeScriptMySQL.js
@@ -50,10 +50,46 @@ const ReactTypeScriptMySql = () => {
       <div className="mb-4">
         <Header />
 
-        <div className="container-fluid my-5 px-5">
-          <h1 className="h1 text-dark">
+        <div className="container my-5 px-5">
+          <h1 className="h1 text-dark text-center">
             React + TypeScript + MySQL + Imgs
           </h1>
+
+          <p className="mt-3">
+            En la presente entrada se explicará como desarrollar un mini proyecto full stack utilizando React con Typescript en el frontend
+            y Node.js con Javascript en el backend. La idea es hacer un registro de usuarios con imágenes las cuales serán almacenadas
+            en la carpeta public del proyecto frontend.
+          </p>
+
+          <h2 className="mt-5">
+            Introducción
+          </h2>
+
+          <p>
+            Para comenzar se deberán crear las carpetas frontend y backend para 
+            tener una estructura de proyecto similar a la siguiente:
+          </p>
+
+          <pre className="language-html">
+            <code>
+              {
+                "el-proyecto/\n" +
+                ">> backend/\n" +
+                ">> frontend/"
+              }
+            </code>
+          </pre>
+
+          <p>
+            Posteriormente, la carpeta frontend deberá ser generada con el comando create-react-app ejecutando:
+            npx create-react-app frontend --template typescript
+            en la carpeta raíz, es decir, ubicado en el nivel de el-proyecto. Con esto ya podremos utilizar typescript con react.
+          </p>
+
+          <p>
+
+          </p>
+
         </div>
 
         <Footer />
